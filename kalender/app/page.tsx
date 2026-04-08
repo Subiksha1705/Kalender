5e291714b662e5dff34a5fdcc44ee5fcc49cfb63
subiksha <subiksharameshkanna@gmail.com>
2026-04-09 04:48:29 +0530

    Remove country onboarding gate

--- kalender/app/page.tsx
+++ kalender/app/page.tsx
@@ -1,12 +1,9 @@
 import WeatherCard from "@/components/weather/WeatherCard";
-import { OnboardingGate } from "@/components/OnboardingGate/OnboardingGate";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#F3E8DE]">
-      <OnboardingGate>
-        <WeatherCard season="summer" />
-      </OnboardingGate>
+      <WeatherCard season="summer" />
     </main>
   );
 }
